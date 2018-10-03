7d767de96dbdeaeb5366fedba29c180a7459df89
Tyrone Turner <c10464@mcast.org.mt>
2018-10-03 12:04:30 +0200

    Ex3

---
+++ APM/src/app/app.component.ts
@@ -0,0 +1,19 @@
+/**The whole code is a component */
+import { Component } from "@angular/core";
+
+/** Below is the Decorator */
+@Component({
+  selector: 'pm-root',
+  template:
+  `<div>
+    <h1>{{pageTitle}}</h1>
+    <pm-products></pm-products>
+  </div>`
+
+})
+
+/** Below is the class */
+export class AppComponent{
+  pageTitle: string = 'Acme Product Management'
+}
+
---
+++ APM/src/app/products/product-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: './product-list.component.html'
+})
+
+export class ProductList{
+    pageTitle: string = 'ProductList'
+    /**getTitle():string{return 'ProductList';}*/
+    
+}
